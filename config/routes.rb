0440fe25badc23e31566f8245fc79c6ad4a888df
k4hun <thameth@gmail.com>
2015-11-24 23:17:46 +0100

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,10 @@ Rails.application.routes.draw do
   end
 
   resources :teachers
-  get 'subjects' => 'reports#subjects'
+  get 'subjects' => 'reports#subjects', as: 'report_subjects'
   resources :visitors
   
   devise_for :users
+
+  root to: 'students#index'
 end
