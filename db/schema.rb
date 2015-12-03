21a444ffdbfcec053102fcf85892ac8a7788de38
k4hun <thameth@gmail.com>
2015-12-03 21:07:30 +0100

    add birthdate to student

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151124211332) do
+ActiveRecord::Schema.define(version: 20151203200211) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "participations", force: :cascade do |t|
     t.integer  "student_id"
     t.integer  "subject_item_id"
@@ -29,6 +31,7 @@ ActiveRecord::Schema.define(version: 20151124211332) do
     t.string   "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "birthdate"
   end
 
   create_table "subject_item_notes", force: :cascade do |t|
