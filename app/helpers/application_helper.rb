5fb94ead813a90e8d2e06b6432a0df6cfaf7b2eb
k4hun <thameth@gmail.com>
2015-12-05 19:20:48 +0100

    fix teachers form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def active?(*paths)
     'active' if paths.any?{ |path| current_page?(path) }
   end
+
+  def subject_items
+    SubjectItem.where(teacher_id: nil)
+  end
 end
